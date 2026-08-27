6d8436487970bfa7d58abca86ccc8bfdecb21a51
keremm44 <keremm.4351@gmail.com>
2026-08-27 17:38:28 +0300

    perf: release transient clipboard image payloads

---
+++ src/WinNotch.UI/Views/ClipboardToastView.Lifecycle.cs
@@ -0,0 +1,26 @@
+using System.Windows;
+
+namespace WinNotch.UI.Views;
+
+public partial class ClipboardToastView
+{
+    protected override void OnInitialized(EventArgs e)
+    {
+        base.OnInitialized(e);
+        IsVisibleChanged += ClipboardToastView_IsVisibleChanged;
+    }
+
+    private void ClipboardToastView_IsVisibleChanged(
+        object sender,
+        DependencyPropertyChangedEventArgs e)
+    {
+        if (IsVisible)
+            return;
+
+        CollapseActions(notify: false);
+        _currentAction = null;
+        _currentImage = null;
+        ImagePreview.Source = null;
+        ImagePreviewBorder.Visibility = Visibility.Collapsed;
+    }
+}
