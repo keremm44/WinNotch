5f844ea3ec08630aa6a8ea3be7981c7448432332
keremm44 <keremm.4351@gmail.com>
2026-08-27 20:40:03 +0300

    feat: add quick peek geometry and timing

--- src/WinNotch.Common/Constants.cs
+++ src/WinNotch.Common/Constants.cs
@@ -12,6 +12,9 @@ public static class Constants
     public const double NotchHoverWidth = 118;
     public const double NotchHoverHeight = 28;
 
+    public const double NotchQuickPeekWidth = 300;
+    public const double NotchQuickPeekHeight = 82;
+
     public const double NotchClipboardWidth = 260;
     public const double NotchClipboardHeight = 40;
     public const double NotchClipboardActionWidth = 300;
@@ -51,6 +54,7 @@ public static class Constants
     public const int ClipboardFlashDurationMs = 2000;
     public const int ScreenshotFlashDurationMs = 2500;
     public const int ContextActionLeaveDelayMs = 900;
+    public const int QuickPeekLeaveGraceMs = 340;
     public const int DropResultDisplayDurationMs = 900;
     public const int MediaAmbientFlashDurationMs = 4000;
     public const int MaxAttentionEventsPerMinute = 5;
