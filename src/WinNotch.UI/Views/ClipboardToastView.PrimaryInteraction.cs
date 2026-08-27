7a924e314d90238a0275cd8f92ef6f804a54b951
keremm44 <keremm.4351@gmail.com>
2026-08-27 20:50:28 +0300

    feat: publish safe actionable clipboard context

--- src/WinNotch.UI/Views/ClipboardToastView.PrimaryInteraction.cs
+++ src/WinNotch.UI/Views/ClipboardToastView.PrimaryInteraction.cs
@@ -1,9 +1,12 @@
 using System.Windows;
+using WinNotch.Common;
 
 namespace WinNotch.UI.Views;
 
 public partial class ClipboardToastView
 {
+    public event EventHandler<LastMeaningfulClipboardContext>? MeaningfulContextAvailable;
+
     internal void RevealActionsFromPrimaryClick()
     {
         Dispatcher.Invoke(() =>
@@ -18,5 +21,43 @@ public partial class ClipboardToastView
             SurfaceMotion.Reveal(ActionPanel, 1.5, 95);
             GetHostWindow()?.SetContextSurfaceExpanded(true);
         });
+    }
+
+    internal void PublishMeaningfulContextIfAvailable()
+    {
+        if (_currentAction == null)
+            return;
+
+        ClipboardContentType contentType;
+        string rawText;
+
+        switch (_currentAction.Kind)
+        {
+            case ContextActionKind.OpenUrl:
+                contentType = ClipboardContentType.Url;
+                rawText = _currentAction.Target;
+                break;
+            case ContextActionKind.ShowInExplorer:
+                contentType = ClipboardContentType.FilePath;
+                rawText = _currentAction.Target;
+                break;
+            case ContextActionKind.ComposeEmail:
+                contentType = ClipboardContentType.Email;
+                rawText = _currentAction.Target.StartsWith("mailto:", StringComparison.OrdinalIgnoreCase)
+                    ? _currentAction.Target[7..]
+                    : _currentAction.Target;
+                break;
+            default:
+                return;
+        }
+
+        MeaningfulContextAvailable?.Invoke(
+            this,
+            new LastMeaningfulClipboardContext(
+                contentType,
+                rawText,
+                rawText,
+                DateTime.Now,
+                _currentAction));
     }
 }
